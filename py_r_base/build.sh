c9cee42c2f505d665eecd57172c26e12edd8fa6c
Ubuntu <ubuntu@ip-172-31-37-17.us-west-2.compute.internal>
2024-04-12 20:14:39 +0000

    py_r_base v1.2

--- py_r_base/build.sh
+++ py_r_base/build.sh
@@ -1,3 +1,3 @@
-sudo docker build -t ghcr.io/marcalva/py_r_base:1.1 .
+sudo docker build -t ghcr.io/marcalva/py_r_base:1.2 .
 
-sudo docker push ghcr.io/marcalva/py_r_base:1.1
+sudo docker push ghcr.io/marcalva/py_r_base:1.2
--- py_r_base/run.sh
+++ py_r_base/run.sh
@@ -1,2 +1,2 @@
 sudo docker run --rm --ipc=host -it --detach-keys="ctrl-@" \
-    -v $HOME:/workspace ghcr.io/marcalva/py_r_base:1.1 /bin/bash
+    -v $HOME:/workspace ghcr.io/marcalva/py_r_base:1.2 /bin/bash
