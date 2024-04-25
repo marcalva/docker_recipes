f420353940fa05f6674b46c001d2792db1e84798
marcalva <marcus.alvarez@ucsf.edu>
2024-04-25 17:34:29 +0000

    v1.3 with magick

--- py_r_base/build.sh
+++ py_r_base/build.sh
@@ -1,3 +1,3 @@
-sudo docker build -t ghcr.io/marcalva/py_r_base:1.2 .
+sudo docker build -t ghcr.io/marcalva/py_r_base:1.3 .
 
-sudo docker push ghcr.io/marcalva/py_r_base:1.2
+sudo docker push ghcr.io/marcalva/py_r_base:1.3
