95f060123bf1c025f99ceee686561e5ff188be9d
marcalva <marcus.alvarez@ucsf.edu>
2024-04-25 17:34:52 +0000

    v1.1 with more r packages

--- sicca/build.sh
+++ sicca/build.sh
@@ -1,3 +1,3 @@
-sudo docker build -t ghcr.io/marcalva/sicca:1.0 .
+sudo docker build -t ghcr.io/marcalva/sicca:1.1 .
 
-sudo docker push ghcr.io/marcalva/sicca:1.0
+sudo docker push ghcr.io/marcalva/sicca:1.1
--- sicca/r_install_pkgs.R
+++ sicca/r_install_pkgs.R
@@ -1,2 +1,8 @@
 repo <- "http://cran.us.r-project.org"
+BiocManager::install("org.Hs.eg.db")
 BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
+BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
+BiocManager::install("TxDb.Hsapiens.UCSC.hg38.knownGene")
+devtools::install_github("immunogenomics/presto")
+devtools::install_github("GreenleafLab/chromVARmotifs")
