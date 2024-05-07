09accdb8d68a3b27db60eb350ad30746d6e86da3
marcalva <marcus.alvarez@ucsf.edu>
2024-05-07 08:22:00 +0000

    added scvi-tools

--- scfm/run.sh
+++ scfm/run.sh
@@ -5,5 +5,8 @@ if type nvidia-smi &> /dev/null; then
     fi
 fi
 
-sudo docker run --rm $gpus --ipc=host -it --detach-keys="ctrl-@" \
-    -v $HOME:/workspace ghcr.io/marcalva/scfm:1.2 /bin/bash
+sudo docker run --rm $gpus --ipc=host -it --detach-keys="ctrl-q" \
+    -v $HOME:/workspace \
+    -v $HOME/.gitconfig:/home/ubuntu/.gitconfig \
+    -v $HOME/.config/github-copilot/:/home/ubuntu/.config/github-copilot/ \
+    $HOME:/workspace ghcr.io/marcalva/scfm:1.2 /bin/bash
