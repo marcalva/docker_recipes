08fafd49a439afcd7c658656b859d33e130d30a5
marcalva <marcus.alvarez@ucsf.edu>
2024-05-07 08:18:55 +0000

    icc

--- micc/r_install_pkgs.R
+++ micc/r_install_pkgs.R
@@ -2,3 +2,10 @@ repo <- "http://cran.us.r-project.org"
 
 remotes::install_cran("languageserver", upgrade = "never")
 remotes::install_cran("GGally", repos = repo, upgrade = "never")
+BiocManager::install("org.Hs.eg.db")
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
+BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
+BiocManager::install("TxDb.Hsapiens.UCSC.hg38.knownGene")
+devtools::install_github("immunogenomics/presto")
+devtools::install_github("GreenleafLab/chromVARmotifs")
--- micc/run.sh
+++ micc/run.sh
@@ -1,2 +1,5 @@
-sudo docker run --rm --ipc=host -it --detach-keys="ctrl-@" \
-    -v $HOME:/workspace ghcr.io/marcalva/micc:1.0 /bin/bash
+sudo docker run --rm --ipc=host -it --detach-keys="ctrl-x" \
+    -v $HOME:/workspace \
+    -v $HOME/.gitconfig:/home/ubuntu/.gitconfig \
+    -v $HOME/.config/github-copilot/:/home/ubuntu/.config/github-copilot/ \
+    ghcr.io/marcalva/micc:1.0 /bin/bash
