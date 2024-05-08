a1875c8b4f93aaa621d65a1c21c82bb973560bba
marcalva <marcus.alvarez@ucsf.edu>
2024-05-08 05:57:55 +0000

    added dnabert2 image

---
+++ dnabert2/build.sh
@@ -0,0 +1,3 @@
+sudo docker build -t ghcr.io/marcalva/dnabert2:1.0 .
+
+sudo docker push ghcr.io/marcalva/dnabert2:1.0
---
+++ dnabert2/run.sh
@@ -0,0 +1,18 @@
+gpus=""
+if type nvidia-smi &> /dev/null; then
+    if nvidia-smi &> /dev/null; then
+         gpus="--gpus all"
+    fi
+fi
+
+sudo docker run --rm $gpus --ipc=host -it --detach-keys="ctrl-x" \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+    -e GH_USERNAME=$GH_USERNAME \
+    -e GH_EMAIL=$GH_EMAIL \
+    -e GH_TOKEN=$GH_TOKEN \
+    -v $HOME:/workspace \
+    -v $HOME/.gitconfig:/home/ubuntu/.gitconfig \
+    -v $HOME/.config/github-copilot/:/home/ubuntu/.config/github-copilot/ \
+    ghcr.io/marcalva/dnabert2:1.0 /bin/bash
