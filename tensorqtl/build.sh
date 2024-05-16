ced0c79d3b1fc4dec8d918060b809c1c504ded61
marcalva <marcus.alvarez@ucsf.edu>
2024-05-16 14:55:42 +0000

    added tensorqtl image

---
+++ tensorqtl/build.sh
@@ -0,0 +1,3 @@
+sudo docker build -t ghcr.io/marcalva/tensorqtl:1.0 .
+
+sudo docker push ghcr.io/marcalva/tensorqtl:1.0
---
+++ tensorqtl/run.sh
@@ -0,0 +1,9 @@
+gpus=""
+if type nvidia-smi &> /dev/null; then
+    if nvidia-smi &> /dev/null; then
+         gpus="--gpus all"
+    fi
+fi
+
+sudo docker run --rm $gpus --ipc=host -it --detach-keys="ctrl-@" \
+    -v $HOME:/workspace ghcr.io/marcalva/tensorqtl:1.0 /bin/bash
