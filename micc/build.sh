6ea050fec655844d465c8d4bfbc568807fd7292f
marcalva <marcus.alvarez@ucsf.edu>
2024-05-30 07:19:55 +0000

    update icc docker

--- micc/build.sh
+++ micc/build.sh
@@ -1,3 +1,3 @@
-sudo docker build -t ghcr.io/marcalva/micc:1.0 .
+sudo docker build -t ghcr.io/marcalva/micc:1.1 .
 
-sudo docker push ghcr.io/marcalva/micc:1.0
+sudo docker push ghcr.io/marcalva/micc:1.1
