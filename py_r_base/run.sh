4f77f94d614b2895212a97df54df90c148f23275
marcalva <marcus.alvarez@ucsf.edu>
2024-10-01 21:03:59 +0000

    escape key and ubuntu base

--- py_r_base/run.sh
+++ py_r_base/run.sh
@@ -1,4 +1,4 @@
-sudo docker run --rm --ipc=host -it --detach-keys="ctrl-@" \
+sudo docker run --rm --ipc=host -it --detach-keys="ctrl-^" \
     -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
--- ubuntu_base/run.sh
+++ ubuntu_base/run.sh
@@ -1,2 +1,2 @@
 sudo docker run --rm --ipc=host -it --detach-keys="ctrl-@" \
-    -v $HOME:/workspace ghcr.io/marcalva/ubuntu_base:1.2 /bin/bash
+    -v $HOME:/workspace ghcr.io/marcalva/ubuntu_base:1.3 /bin/bash
